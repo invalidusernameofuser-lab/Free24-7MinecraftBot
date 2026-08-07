75775b24ee034cbcf84254e13956860459298cc7
invalidusernameofuser-lab <invalidusernameofuser@gmail.com>
2026-08-07 09:28:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
   host: process.env.SERVER_IP,       // Railway ke Environment Variables se IP lega
-  port: parseInt(process.env.SERVER_PORT) || 25565,
-  username: process.env.BOT_NAME || 'YourFree24/7Bot',
+  port: parseInt(process.env.SERVER_PORT) || 19120,
+  username: process.env.BOT_NAME || 'SAYYAR_KO_BANA_DIA_MAMO',
   auth: 'offline'
 })
 
